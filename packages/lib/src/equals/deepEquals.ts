75910997b549738f12a608a2c3ae3acad984371d
HYOJIN <hyo.khj@gmail.com>
2025-07-24 23:19:14 +0900

    feat: shallowEquals/deepEquals 코드 개선

--- packages/lib/src/equals/deepEquals.ts
+++ packages/lib/src/equals/deepEquals.ts
@@ -1,34 +1,21 @@
 export const deepEquals = (a: unknown, b: unknown) => {
   if (a === b) return true;
 
-  if (a !== null && b !== null) {
-    // 배열 비교 (재귀)
-    if (Array.isArray(a) && Array.isArray(b)) {
-      if (a.length === b.length && a.every((item, index) => deepEquals(item, b[index]))) {
-        return true;
-      }
-      return false;
-    }
+  if (typeof a !== "object" || a === null || typeof b !== "object" || b === null) return false;
 
-    // 객체 비교 (재귀)
-    if (typeof a === "object" && typeof b === "object") {
-      const objA = a as Record<string, unknown>;
-      const objB = b as Record<string, unknown>;
+  const objA = a as Record<string, unknown>;
+  const objB = b as Record<string, unknown>;
 
-      const keysA = Object.keys(objA);
-      const keysB = Object.keys(objB);
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
 
-      if (keysA.length !== keysB.length) {
-        return false;
-      }
-
-      for (const key of keysA) {
-        if (!deepEquals(objA[key], objB[key])) return false;
-      }
-
-      return true;
-    }
+  if (keysA.length !== keysB.length) {
+    return false;
   }
 
-  return false;
+  for (const key of keysA) {
+    if (!deepEquals(objA[key], objB[key])) return false;
+  }
+
+  return true;
 };
